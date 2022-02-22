cc0071ded50e0fd1d6c189af8b869143ca6bcca0
No-Agent-2990 <100043573+No-Agent-2990@users.noreply.github.com>
2022-02-21 22:42:35 -0500

    Update network.h

--- arduino/linear_drive/network.h
+++ arduino/linear_drive/network.h
@@ -1,10 +1,10 @@
 // Network credentials and IP
 
-// STA defines used if in station mode
+// STA defines used if in station mode. This would be the SSID and PWD of the network to connect *to*.
 #define STA_HOSTAP_SSID   "HOSTAPSSID"
 #define STA_HOSTAP_PWD    "password"
 
-// AP defines used if acting as AP
+// AP defines used if acting as AP. This is the SSID and PWD to connect to this unit.
 #define AP_SSID           "APSSID"
 #define AP_PWD            "appassword"
 #define AP_IPADDR         IPAddress(192,168,1,100)
